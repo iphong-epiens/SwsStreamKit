37f596c5cd4c58b2588f31b267cbbfd6671de0f0
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-27 12:18:25 +0900

    Haishinkit 추가

--- Package.swift
+++ Package.swift
@@ -12,12 +12,15 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwsStreamKit",
-            targets: ["SwsStreamKit"]),
+            targets: ["HaishinKit"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(name: "SwsStreamKit",
-                      path:"./Sources/SwsStreamKit.xcframework")
+                      path:"./Sources/SwsStreamKit.xcframework"),
+        
+        .binaryTarget(name: "HaishinKit",
+                      path:"./Sources/HaishinKit.xcframework")
     ]
 )
