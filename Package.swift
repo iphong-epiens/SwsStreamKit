5352f36732a8a5966e8378d794a4baeecdee1839
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-27 22:23:02 +0900

    swsstreamkit 라이브러리를 static으로 변경

--- Package.swift
+++ Package.swift
@@ -9,15 +9,26 @@ let package = Package(
       .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "SwsStreamKit",
-            targets: ["SwsStreamKit"]),
+        // Products define the executables and libraries a package produces, and make them visible to other packages.xx
+        .library(name: "SwsStreamKit",
+                 //type: .dynamic,
+                 targets: ["SwsStreamKit", "SwsStreamKitFramework", "HaishinKit","Logboard"])
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "SwsStreamKit",
-                      path:"./Sources/SwsStreamKit.xcframework")
+        .target(name: "SwsStreamKit",
+                dependencies: []),
+        
+        .binaryTarget(name: "SwsStreamKitFramework",
+                      path:"./Sources/Frameworks/SwsStreamKit.xcframework"),
+        
+        .binaryTarget(
+            name: "HaishinKit",
+            path: "./Sources/Frameworks/HaishinKit.xcframework"),
+        
+        .binaryTarget(
+            name: "Logboard",
+            path: "./Sources/Frameworks/Logboard.xcframework")
     ]
 )
