dd0916d3243e40f10de5bb876cd0159f7415c2b2
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-27 12:58:27 +0900

    Logboard 라이브러리 참조 추가

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwsStreamKit",
-            targets: ["SwsStreamKit", "HaishinKit"]),
+            targets: ["SwsStreamKit", "HaishinKit", "Logboard"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
