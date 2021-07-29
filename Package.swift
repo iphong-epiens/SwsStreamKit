bb5ac223f7ec64cbd40150f7015afeebcd4f1746
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-29 16:54:52 +0900

    프레임워크 변경

--- Package.swift
+++ Package.swift
@@ -15,29 +15,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "SwsStreamKit",
-                dependencies: ["Framework", "RxSwift", "RxCocoa", "RxCocoaRuntime", "RxRelay","HaishinKit","Logboard"]),
-
-        .binaryTarget(name: "Framework",
-                      path: "./Sources/SwsStreamKit.xcframework"),
-        
-        .binaryTarget(name: "RxSwift",
-                      path: "./Sources/RxSwift.xcframework"),
-        
-        .binaryTarget(name: "RxCocoa",
-                          path: "./Sources/RxCocoa.xcframework"),
-        
-        .binaryTarget(name: "RxCocoaRuntime",
-                      path: "./Sources/RxCocoaRuntime.xcframework"),
-        
-        .binaryTarget(name: "RxRelay",
-                      path: "./Sources/RxRelay.xcframework"),
-        
-     
-        .binaryTarget(name: "HaishinKit",
-                      path: "./Sources/HaishinKit.xcframework"),
-        
-        .binaryTarget(name: "Logboard",
-                      path: "./Sources/Logboard.xcframework")
+        .binaryTarget(name: "SwsStreamKit",
+                      path: "./Sources/SwsStreamKit.xcframework")
     ]
 )
