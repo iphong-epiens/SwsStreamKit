c1ce44d6e97b812f13e58d5b63c518f934f717c6
Inpyo Hong <y2kpaulh@epiens.com>
2021-07-29 11:23:20 +0900

    RxSwift, HaishinKit 관련 프레임워크 추가

--- Package.swift
+++ Package.swift
@@ -5,19 +5,39 @@ import PackageDescription
 
 let package = Package(
     name: "SwsStreamKit",
-    platforms: [
-      .iOS(.v14)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwsStreamKit",
             targets: ["SwsStreamKit"]),
     ],
+   
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "SwsStreamKit",
-                      path:"./Sources/SwsStreamKit.xcframework")
+        .target(name: "SwsStreamKit",
+                dependencies: ["Framework", "RxSwift", "RxCocoa", "RxCocoaRuntime", "RxRelay","HaishinKit","Logboard"]),
+
+        .binaryTarget(name: "Framework",
+                      path: "./Sources/SwsStreamKit.xcframework"),
+        
+        .binaryTarget(name: "RxSwift",
+                      path: "./Sources/RxSwift.xcframework"),
+        
+        .binaryTarget(name: "RxCocoa",
+                          path: "./Sources/RxCocoa.xcframework"),
+        
+        .binaryTarget(name: "RxCocoaRuntime",
+                      path: "./Sources/RxCocoaRuntime.xcframework"),
+        
+        .binaryTarget(name: "RxRelay",
+                      path: "./Sources/RxRelay.xcframework"),
+        
+     
+        .binaryTarget(name: "HaishinKit",
+                      path: "./Sources/HaishinKit.xcframework"),
+        
+        .binaryTarget(name: "Logboard",
+                      path: "./Sources/Logboard.xcframework")
     ]
 )
